7a8c9d38983dbeab38a598fbb2dced9def48716c
LEOPOLDO PRADO PEREZ <99229005+lprado-ekt@users.noreply.github.com>
2023-04-05 13:41:09 -0600

    Commit de los últimos cambios de 05/04/2023

---
+++ Calculador.py
@@ -0,0 +1,22 @@
+class Calculador:
+    division = ''
+    suma = ''
+    def print_division(self):
+        print(self.division)
+
+    def print_suma(self):
+        print(self.suma)
+
+
+operacion = Calculador()
+
+print(type(Calculador))
+
+print(operacion)
+
+
+jorge = Person()
+jorge.name = 'Jorge'
+jorge.school = 'Universidad de la vida'
+jorge.print_name()
+jorge.print_school()
---
+++ Ejemplo.py
@@ -0,0 +1,29 @@
+class Calculadora:
+    num1 = 0
+    num2 = 0
+
+    def __init__(self, num1, num2):
+        self.num1 = float(num1)
+        self.num2 = float(num2)
+
+    def sumar(self):
+        suma = self.num1+self.num2
+        print("La suma es: ", suma)
+
+    def restar(self):
+        resta = self.num1-self.num2
+        print("La resta es: ", resta)
+
+    def multiplicar(self):
+        multiplicacion = self.num1*self.num2
+        print("La multiplicación es: ", multiplicacion)
+
+    def dividir(self):
+        divicion = self.num1/self.num2
+        print("La divición es: ", divicion)
+
+
+num1 = input("Primer número: ")
+num2 = input("Segundo numero: ")
+
+calculadora = Calculadora(num1, num2)
